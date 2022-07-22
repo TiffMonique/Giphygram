05b707e3657f9e8620919f3c55da0abd53eb9509
TiffMonique <tiffanymatamoros72@gmail.com>
2022-07-21 21:54:01 -0600

    Primer commit

---
+++ giphygram/src/components/Cards/Cards.tsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+// Chakra imports
+import {
+  Flex,
+  Button,
+  Icon,
+  Image,
+  useColorModeValue,
+  SimpleGrid,
+  boxBg,
+} from "@chakra-ui/react";
+// Assets
+
+import { IoHeartOutline } from "react-icons/io5";
+
+const Cards = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  
+
+  let iconBox = useColorModeValue("gray.100", "whiteAlpha.200");
+  let iconColor = useColorModeValue("brand.200", "white");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+          params: {
+            api_key: "RUDZVfvwwZNVQVTAHDYpcIeIN0jd6h6T",
+            limit: 100,
+          },
+        });
+
+        console.log(results);
+        setData(results.data.data);
+      } catch (err) {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 4000);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const renderGifs =()=>{
+    return data.map((element) => {
+      return (
+          <div key={element.id} className="gif">
+           
+    
+      <Flex
+        borderRadius="20px"
+      
+        p="20px"
+        h="345px"
+        w={{ base: "315px", md: "345px" }}
+        alignItems="center"
+        direction="column"
+      >
+        <Flex w="100%" mb="18px">
+          <Button
+            w="38px"
+            h="38px"
+            textAlign="center"
+            justifyContent="center"
+            borderRadius="12px"
+            me="12px"
+            bg={iconBox}
+          >
+            <Icon
+              w="24px"
+              h="24px"
+              as={IoHeartOutline}
+              color={iconColor}
+            />
+          </Button>
+        </Flex>
+        <Image
+          src={element.images.fixed_height.url}
+          maxW="100%"
+          maxH="100%"
+          borderRadius="20px"
+          mb="10px"
+        />
+        
+      </Flex>
+    
+
+          </div>
+
+      
+      )
+    });
+  }
+
+  return (
+    <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+     {renderGifs()}
+    </SimpleGrid>
+   
+  );
+};
+
+export default Cards;
